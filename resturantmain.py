a541fae3b4f07929094c11e598f5fee81f1f810f
jharris49 <22jharris@wakatipu.school.nz>
2025-04-16 18:32:55 +1200

    Base Version 3
    Added functionality means that the of the system is now able to print a customers order, confirm a customers order, and get the total cost of the customers order. This functionality has come through the creation of multiple functions.
    
    The functions that have been added that create this functionality are: print_order, get_price, and confirm order.
    
    Minor bug fixes regarding the programs input validation and some of its functionality. One known bug still to be fixed.

--- resturantmain.py
+++ resturantmain.py
@@ -1,3 +1,4 @@
+#imports modules including my own input validation one that checks if user input
 import inputvalidation, time, os, sys
 
 restaurant_menu = [
@@ -23,8 +24,10 @@ meal_contents = {
 def menu(iteration):
     """
     """
+#checks what menu is applicable for the operators situation and acts accordingly
     if iteration == 1:
-        menu_choice = inputvalidation.pos_int("1. - Add meal to order\n2. - Exit\n", "Please enter a valid choice (1, 2, or 3)")
+        menu_choice = inputvalidation.pos_int("1. - Add meal to order\n2. - Exit\n", "Please enter a valid choice (1 or 2)")
+        #calls function that acts accordingly to what the operator asks for
         evaluate_input(menu_choice, 1)
     elif iteration == 2:
         menu_choice = inputvalidation.pos_int("1. - Add meal to order\n2. - Edit order\n3. - Print order\n4. - Confirm order\n5. - Exit\n", "Please enter a valid choice (1, 2, 3, 4, or 5)")
@@ -33,15 +36,31 @@ def menu(iteration):
 def edit_order():
     print("Edit order")
 
+
+def print_menu():
+    """
+    """
+    printable_menu = ""
+    meal_counter = 1
+#for each list in restaurant menu it loops
+    for item in restaurant_menu:
+        #saves a number that represents a meal then saves the according meal and price from restaurant menu
+        printable_menu += (f"{meal_counter}. - ${item[1]} {item[0]}\n")
+        meal_counter += 1
+    return printable_menu
+
+
 def add_meal():
     """
     """
-    os.system("clear")
-    print_menu()
-    wanted_meal = inputvalidation.pos_int("", "Please entere a valid meal option")
+    #creates a variable that saves what meal the operator wants 
+    wanted_meal = inputvalidation.pos_int(print_menu(), "Please enter a valid meal option")
     while True:
+        #checks what meal the operator has selected and acts accordingly
         if wanted_meal == 1:
+            #saves the chosen meal from restaurant menu to a variable 
             selected_meal = restaurant_menu[0]
+            #adds this meal to the customers order
             order.append(selected_meal)
             print(order)
             break
@@ -68,28 +87,69 @@ def add_meal():
 def edit_meal():
     print("Edit Meal")
 
-def get_price():
-    print("Get price")
 
-def print_menu():
-    """
-    """
-    meal_counter = 1
-    for item in restaurant_menu:
-        print(f"{meal_counter}. - ${item[1]} {item[0]}")
-        meal_counter += 1
+def get_price():
+    total_price = 0.00
+    for item in order:
+        total_price += item[1]   
+    total_price = round(total_price, 2)
+    return total_price
+
 
 def print_order():
-    print("Print order")
+    """
+    """
+    os.system("clear")
+    order.sort()
+    total_cost = get_price()
+    print("Restaurant Name")
+    print("Order:") 
+#for each list in restaurant menu it loops
+    for item in order:
+        meal_quantity = order.count(item)
+        if meal_quantity != 1:
+            for i in range(meal_quantity - 1):
+                order.remove(item)
+                i += 1
+        #prints a number that represents then prints out the according meal and price from user order
+        print(f"X{str(meal_quantity):2s}{item[0]:10s}${item[1]}")
+    print("-----------------")
+    print(f"{"Total:":13}${total_cost}\n")
+
 
 def confirm_order():
-    print("Confirm order")
+    """
+    """
+    os.system("clear")
+    while True:
+        #asks operator if they want to confirm order, saves this to variable
+        check_confirmation= inputvalidation.pos_int("Are you sure you want to confirm the order?\n1. - Yes\n2. - No\n", "Please enter a valid option (1 or 2)")
+#checks if the operator wants to confirm order or not
+        if check_confirmation == 1:
+            print("Order confirmed")
+            #prints customer order
+            print_order()
+            continue_on = input("Hit return to continue")
+            order.clear()
+            os.system("clear")
+            menu(1)
+            break
+        elif check_confirmation == 2:
+            os.system("clear")
+            menu(2)
+            break
+        else:
+            print("You entered an invalid option, please try again")
+            time.sleep(2)
+
 
 def evaluate_input(choice, iteration):
     """
     """
+#checks what iteration/situation needs to be evaluated
     if iteration == 1:
         while True:
+        #checks if operator input and acts accordingly
             if choice == 1:
                 add_meal()
                 break
@@ -97,9 +157,12 @@ def evaluate_input(choice, iteration):
                 sys.exit("The program has ended")
             else:
                 print("You entered a choice that does not exist, please try again")
+                #returns to main menu
                 menu(1)
+                break
     elif iteration == 2:
         while True:
+         #checks if operator input and acts accordingly
             if choice == 1:
                 add_meal()
                 break
@@ -118,8 +181,9 @@ def evaluate_input(choice, iteration):
                 os.system("clear")
                 print("You entered a choice that does not exist, please try again")
                 menu(2)
+                break
 
-
+os.system("clear")
 menu(1)
 
 """
